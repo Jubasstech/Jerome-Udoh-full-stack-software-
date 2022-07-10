16e1e15cbab22b52c61008b80a94646fbd9f1b84
Jerome Bassey Udoh <jeromeudoh7@gmail.com>
2022-07-10 11:35:45 -0700

    Write a function that prints

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,9 @@
 /**
 * print_alphabet_x10 - a function that prints 10 times the alphabet
 *
-* Return: x10 a-z 
-*/void print_alphabet_x10(void)
+* Return: x10 a-z
+*/
+void print_alphabet_x10(void)
 {
 int n, co;
 co = 0;
