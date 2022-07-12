6037f45c12216416bf8706b2b99302b4a1a5e710
Jerome Bassey Udoh <jeromeudoh7@gmail.com>
2022-07-12 08:35:55 -0700

    Write a function that copies the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* char *_strcpy - a function that copies the string pointed to by src
+ * char *_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
  * @src: copy from
  * Return: string
